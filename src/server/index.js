6932a15c8d8545008cba34fc5f06e0b188729593
‪Moamen Mohasseb‬‏ <76065178+moamen-mohasseb@users.noreply.github.com>
2021-05-12 22:54:48 +0200

    Delete old

--- src/server/index.js
+++ src/server/index.js
@@ -31,15 +31,7 @@ app.post('/geturl', async (req, res) => {
     console.log('server response: Done');
     const data = await response.json();
     console.log('server side sample: ', data);
-    /*const projectData = {
-      model:data.model,      
-       text:data.sentence_list[0].text,
-       score_tag: data.score_tag,
-       confidence: data.confidence,
-       irony: data.irony,
-       subjectivity: data.subjectivity
-
-     };*/
+    
      try{res.send(data);}
      catch(error){console.log(error)}
      //console.log("Opject to send: ",projectData);
